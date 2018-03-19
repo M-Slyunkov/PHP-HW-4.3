3cf566457ff59368344ac33c3917d7cc2dcd227a
Maksim <36380100+M-Slyunkov@users.noreply.github.com>
2018-03-19 23:24:45 +0300

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+//$servername = "localhost";
+//$dbname = "ptrofimov";
+//$username = "ptrofimov";
+//$password = "neto1216";
+//$cookie_expiration_time = time() + 60 * 15;
+
+$servername = "localhost";
+$dbname = "ptrofimov";
+$username = "ptrofimov";
+$password = "neto1216";
+$cookie_expiration_time = time() + 60 * 15;
---
+++ logout.php
@@ -0,0 +1,3 @@
+<?php
+setcookie("logged_in", '',time() -1);
+header("Location: index.php");
